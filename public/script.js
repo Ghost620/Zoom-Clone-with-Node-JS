a83e260afbf8c5d63e1840f3545ee1c4571fac57
Ghost620 <faghost6201@gmail.com>
2022-08-31 20:44:46 +0500

    initialized

--- public/script.js
+++ public/script.js
@@ -13,6 +13,7 @@ navigator.mediaDevices.getUserMedia({
     addVideoStream(myVideo, stream)
 })
 
+socket.emit('join-room')
 
 const addVideoStream = (video, stream) => {
     video.srcObject = stream
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const { Socket } = require('socket.io')
 const app = express()
 const server = require('http').Server(app)
+const io = require('socketio')(server)
 const { v4 : uuidv4 } = require('uuid')
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -11,6 +13,12 @@ app.get('/', (req, res) => {
 
 app.get('/:room', (req, res) => {
     res.render('room', {roomId: req.params.room})
+})
+
+io.on('connection', socket => {
+    socket.on('join-room', () => {
+        console.log('Room Joined')
+    })
 })
 
 server.listen(3030)
